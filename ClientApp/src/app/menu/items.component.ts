87289f8c21df4ed13f322e0b719f0bbfbfccd3c6
ThiboTouch <lethabo.mbokazi@gmail.com>
2020-03-15 16:49:33 +0200

    fix dupication of menu items

--- ClientApp/src/app/menu/items.component.ts
+++ ClientApp/src/app/menu/items.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Repository } from "../models/repository";
 import { Recipe } from "../models/recipe.model";
 
@@ -8,14 +8,28 @@ import { Recipe } from "../models/recipe.model";
 })
 
 export class ItemsComponent {
+  newArray = [];
 
   constructor(private repo: Repository) {
     this.repo.getRecipes();
   }
 
   get recipes(): Recipe[] {
+    this.populateNewArray();
     if (this.repo.recipes != null && this.repo.recipes.length > 0) {
       return this.repo.recipes;
     }
+  }
+
+  populateNewArray() {
+    for (let i = 0; i < this.repo.recipes?.length; i += 3) {
+      if (this.newArray.length < this.checkLength()) {
+        this.newArray.push({ items: this.repo.recipes.slice(i, i + 3) });
+      }
+    }
+  }
+
+  checkLength() {
+    return Math.ceil(this.repo.recipes.length / 3);
   }
 }
